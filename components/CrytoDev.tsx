4997b7939f560376be2763e69e123160b2475f60
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 12:57:34 +0100

    added the presaleMint function to  mint an Crypto Dev Nft

--- components/CrytoDev.tsx
+++ components/CrytoDev.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import {providers,utils,Contract} from "ethers"
-import { useEffect,useState,useRef } from 'react'
-import web3Modal from "web3modal"
-import styles from "../styles/Home.module.css"
-import {crytoDevGoerliAddress,crytoDevPolygonAddress} from "../constants"
-import {abi} from "../constants/contractmetadata.json"
+import React from "react";
+import { providers, utils, Contract } from "ethers";
+import { useEffect, useState, useRef } from "react";
+import web3Modal from "web3modal";
+import styles from "../styles/Home.module.css";
+import { crytoDevGoerliAddress, crytoDevPolygonAddress } from "../constants";
+import { abi } from "../constants/contractmetadata.json";
 
-type Props = {}
+type Props = {};
 
-type alertType={
-  message? : string
-}
+type alertType = {
+  message?: string;
+};
 
-type needSigner = boolean
-
+type needSigner = boolean;
 
 const CrytoDev = ({}: Props) => {
   // walletConnected keep track of whether the user's wallet is connected or not
@@ -31,50 +30,61 @@ const CrytoDev = ({}: Props) => {
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef<any>();
 
+  const getSignerOrProvider = async (needSigner: needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
 
-  const getSignerOrProvider = async(needSigner:needSigner =false)=>{
-     const provider = await web3ModalRef.current.connect()
-     const web3Provider = new providers.Web3Provider(provider)
-     const {chainId} = await web3Provider.getNetwork()
+    if (chainId != 5 || 80001) {
+      alert("Please change network to goerli or polygon");
+      throw new Error("Please change network to goerli or polygon");
+    }
 
-     if(chainId !=5 || 80001){
-         alert("Please change network to goerli or polygon")
-         throw new Error("Please change network to goerli or polygon")
-     }
+    if (needSigner) {
+      const signer = await web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
 
-     if(needSigner){
-        const signer = await web3Provider.getSigner()
-        return signer
-     }
-     return web3Provider
-  }
+  const getSignerConnectedContract = async (): Promise<Contract> => {
+    const signer = await getSignerOrProvider(true);
+    const signerConnectedContract = new Contract(
+      crytoDevGoerliAddress,
+      abi,
+      signer
+    );
+    return signerConnectedContract;
+  };
 
-  const getSignerConnectedContract = async():Promise<Contract> =>{
-       const signer = await getSignerOrProvider(true)
-       const signerConnectedContract = new Contract(crytoDevGoerliAddress,abi,signer)
-       return signerConnectedContract 
-  }
+  const getProviderConnectedContract = async (): Promise<Contract> => {
+    const provider = await getSignerOrProvider(false);
+    const providerConnectedAccount = new Contract(
+      crytoDevGoerliAddress,
+      abi,
+      provider
+    );
+    return providerConnectedAccount;
+  };
 
-  const getProviderConnectedContract = async():Promise<Contract>=>{
-     const provider = await getSignerOrProvider(false)
-     const providerConnectedAccount = new Contract(crytoDevGoerliAddress,abi,provider)
-     return providerConnectedAccount
-  }
+  const presaleMint = async () => {
+    try {
+      const nftContract = await getSignerConnectedContract();
+      const tx = await nftContract.presaleMint({
+        value: utils.parseEther("0.01"),
+      });
+      // to load te button when the transaction is about to be performed
+      setLoading(true)
+      await tx.wait()
+      // to stop the button from after the transaction response has been gotten
+      setLoading(false)
+      alert("you have successful minted a Crypto Dev Nft , welldone 🚀🚀")
+    } catch (e: unknown) {
+      console.log(e);
+    }
+  };
 
+  return <div>helfahhh</div>;
+};
 
-
-
-
-  const presaleMint =async () => {
-       
-  }
-
-
-  return (
-    <div>
-        helfahhh
-    </div>
-  )
-}
-
-export default CrytoDev
+export default CrytoDev;
