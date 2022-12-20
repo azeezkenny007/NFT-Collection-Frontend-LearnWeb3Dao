77b101c7f6e27dfacbc103c0978b5f2ba9c68edd
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 12:02:08 +0100

    imported the web3modal library into the Crytodev

--- components/CrytoDev.tsx
+++ components/CrytoDev.tsx
@@ -1,5 +1,8 @@
-
 import React from 'react'
+import {providers,utils,Contract} from "ethers"
+import { useEffect,useState,useRef } from 'react'
+import web3Modal from "web3modal"
+import styles from "../styles/Home.module.css"
 
 type Props = {}
 
