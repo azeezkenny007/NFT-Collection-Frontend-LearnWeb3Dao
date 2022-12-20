c4776826b91b1f6ca676d97e0932ad74d724a64c
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-20 12:07:23 +0100

    added the useState that will monitor the state changes

--- components/CrytoDev.tsx
+++ components/CrytoDev.tsx
@@ -6,10 +6,24 @@ import styles from "../styles/Home.module.css"
 
 type Props = {}
 
-const CrytoDev = (props: Props) => {
+const CrytoDev = ({}: Props) => {
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState<boolean>(false);
+  // presaleStarted keeps track of whether the presale has started or not
+  const [presaleStarted, setPresaleStarted] = useState<boolean>(false);
+  // presaleEnded keeps track of whether the presale ended
+  const [presaleEnded, setPresaleEnded] = useState<boolean>(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState<boolean>(false);
+  // checks if the currently connected MetaMask wallet is the owner of the contract
+  const [isOwner, setIsOwner] = useState<boolean>(false);
+  // tokenIdsMinted keeps track of the number of tokenIds that have been minted
+  const [tokenIdsMinted, setTokenIdsMinted] = useState<string>("0");
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef();
   return (
     <div>
-     hello my people
+        hello my people
     </div>
   )
 }
