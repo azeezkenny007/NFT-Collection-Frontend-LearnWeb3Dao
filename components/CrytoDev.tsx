9429e7cb6bfbe0c42fa5d880870e2322e3fb0e5d
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-27 16:35:46 +0100

    added the needed modifications

--- components/CrytoDev.tsx
+++ components/CrytoDev.tsx
@@ -5,7 +5,7 @@ import Web3Modal from "web3modal";
 import styles from "../styles/Home.module.css";
 import { crytoDevGoerliAddress, crytoDevPolygonAddress } from "../constants";
 import { abi } from "../constants/contractmetadata.json";
-
+import { Fade } from "react-awesome-reveal";
 
 type Props = {};
 
@@ -334,9 +334,9 @@ const CrytoDev = ({}: Props) => {
           {renderButton()}
         </div>
         <div>
-        
+          <Fade>
           <img className={styles.image} src="./image.svg" />
-         
+          </Fade>
         </div>
       </div>
     </div>
